46a965763a373081044df6e6df9ebf89a5b87e01
Carlos <carlosalbertosantos@uts.edu.co>
2023-05-29 17:55:17 -0500

    Actualizacion

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
-
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
-app.get('/', (req,res) => res.send('Welcome to the server aws, with express'));
+app.get('/', (req, res) => {
+  const filePath = path.join(__dirname, 'src', 'index.html');
+  res.sendFile(filePath);
+});
 
-app.listen(3000);
-console.log('server on port 3000');
+app.get('/main.html', (req, res) => {
+  const filePath = path.join(__dirname, 'src', 'main.html');
+  res.sendFile(filePath);
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
